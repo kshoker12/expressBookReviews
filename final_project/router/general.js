4cf21d329cbd358524b5fe53b5e0ba406ce5f80d
Karandeep Shoker <141450985+kshoker12@users.noreply.github.com>
2023-08-24 21:23:03 -0700

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,28 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let bookKeys = Object.keys(books);
+  var booksbyauthor = new Array(bookKeys.length);
+  for (var i = 0; i < bookKeys.length; i++) {
+      let curr = books[bookKeys[i]];
+      if (curr.author === author) {
+          booksbyauthor[i] = curr;
+      }
+  }
+  booksbyauthor = booksbyauthor.filter((user)=> user != null);
+  res.send(JSON.stringify(booksbyauthor, null, 4));
 });
 
 // Get all books based on title
@@ -36,7 +44,7 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
+  //Write your code heredf
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
